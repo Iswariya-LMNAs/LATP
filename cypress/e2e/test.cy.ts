98e5ee2a4b791588d3767852de6364030bd4a87b
varshenilmnas <varsheni.velmurugan@lmnas.com>
2025-06-13 18:11:56 +0530

    Updated testfile and config

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,57 +1,57 @@
-import  { clActionFactory } from "../../src/action"; 
+import { clActionFactory } from "../../src/action";
 import { fnGetDelay } from "../../src/delay";
-
+// import { fnCreateTestRunIfLabExists, fetchData, type ifScript } from "../../src/fetchLab";
 /// <reference types="cypress" />
-
-/**
- * @describe
- *  Fetch the test data through API and store in "TtestHeaderData"
- * @it 
- * Logs into the application, navigates to the quotation page.
- * Filter the actions from the "TtestHeaderData".
- * Perform the executeAction in class clAction for filtered actions
- */
-
-describe("Testing API Data", () => {
+describe("Testing Test Lab", () => {
+   // sample output :
+  //   // [{"test_script": "Sample", "master_data": "Master Data-1"}, {"test_script": "Sample-2", "master_data": "Master Data-2"}]
+  let testData: any[] = [];
   before(() => {
-    cy.task("fetchData").then((TtestHeader :TtestHeaderData) => {
-      cy.wrap(TtestHeader).as('TtestHeader'); 
+    cy.task("fetchMasterData").then((result: { message: { scripts_data: any[] } }) => {
+      testData = result.message.scripts_data;
+      cy.wrap(testData).as("scripts");
+      // cy.log("Fetched Scripts Data: " + JSON.stringify(testData));
     });
   });
-
-  it("login to the url and should log full API test data", () => {   
-    const targetUrl = Cypress.env("TARGET_URL");
-    const loginEmail = Cypress.env("LOGIN_EMAIL");
-    const loginPassword = Cypress.env("LOGIN_PASSWORD");
-    const targetPath = Cypress.env("TARGET_PATH")
-    cy.request({
-      method: 'POST',
-      url: `${targetUrl}/api/method/login`,
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: {
-        usr: loginEmail,
-        pwd: loginPassword
-      }
-    }).then((response: Cypress.Response<any>) => {
-      cy.log(JSON.stringify(response.body));
-      // Now TypeScript knows response.body exists
-    });
-    cy.visit(`${targetUrl}/app/home`);
-    cy.get('.btn-primary').click()
-    cy.visit(`${targetUrl}/app/${targetPath}`); 
-    cy.get(".primary-action").click();
-    cy.task("fetchData").then((TtestHeader : TtestHeaderData) => {
-      //Filter all header actions (Onload,On change ,OnTab...) from TtestHeaderData 
-      const CaFilteredActions :TTactionsData = TtestHeader.test_fields.filter(lActionRow => lActionRow.action); 
+it("loops through test data", function () {
+  const targetUrl = Cypress.env("TARGET_URL");
+  const loginEmail = Cypress.env("LOGIN_EMAIL");
+  const loginPassword = Cypress.env("LOGIN_PASSWORD");
+  const targetPath = Cypress.env("TARGET_PATH")
+  cy.get("@scripts").then((scripts: TtestHeaderData[]) => {
+    scripts.forEach((script) => {
+      cy.log(JSON.stringify(script));
+      cy.visit(`${targetUrl}/login#login`); //v-14
+      cy.get("#login_email").type(`${loginEmail}`);
+      cy.get("#login_password").type(`${loginPassword}{enter}`).wait(fnGetDelay("medium"));
+      cy.wait(fnGetDelay("medium"));
+      cy.visit(`${targetUrl}/app/${targetPath}`);
+      cy.wait(fnGetDelay("medium"));
+      cy.get(".primary-action").click();
+      cy.wait(fnGetDelay("medium"));
+      console.log(script);
+        //Filter all header actions (Onload,On change ,OnTab...) from TtestHeaderData
+      const CaFilteredActions: TTactionsData = script.test_fields.filter((lActionRow) => lActionRow.action);
+      console.log("CaFilteredActions:", CaFilteredActions);
+      cy.log("cafiletr",CaFilteredActions)
       CaFilteredActions.forEach((lActionRow) => {
-      const CaActionData: TTactionsData = clActionFactory.filterActionData(TtestHeader.test_fields, lActionRow)
-      var loAction:ifActionHandler = clActionFactory.createAction(lActionRow.action, CaActionData)  
-      loAction.executeAction() 
-      });
+      const CaActionData: TTactionsData = clActionFactory.filterActionData(script.test_fields, lActionRow);
+      const loAction: ifActionHandler = clActionFactory.createAction(lActionRow.action, CaActionData);
+      loAction.executeAction();
+    });
+      // logout
+      cy.get('.nav-link > .avatar > .avatar-frame').click();
+        cy.wait(fnGetDelay("long"));
+        cy.get('[onclick="return frappe.app.logout()"]').click();
+        cy.wait(3000);
+        cy.clearCookies();
+        cy.clearLocalStorage();
     });
   });
 });
- 
+});
+
+
+
+
+
